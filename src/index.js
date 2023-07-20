3817bc88d94fe99389e50e7ec267d573077d7afa
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-07-20 18:32:44 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 console.log('%c HI', 'color: firebrick')
+
+window.onload = function() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+      .then(response => response.json())
+      .then(data => {
+        let dogImgContainer = document.getElementById('dog-image-container');
+        data.message.forEach(imageUrl => {
+          let img = document.createElement('img');
+          img.src = imageUrl;
+          dogImgContainer.appendChild(img);
+        });
+      });
+  }
+
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let dogBreedContainer = document.getElementById('dog-breeds');
+    for(let breed in data.message){
+      let li = document.createElement('li');
+      li.innerHTML = breed;
+      dogBreedContainer.appendChild(li);
+    }
+  });
+
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let dogBreedContainer = document.getElementById('dog-breeds');
+    for(let breed in data.message){
+      let li = document.createElement('li');
+      li.innerHTML = breed;
+      dogBreedContainer.appendChild(li);
+    }
+  });
+
+  for(let breed in data.message){
+    let li = document.createElement('li');
+    li.innerHTML = breed;
+    li.addEventListener('click', function() {
+      this.style.color = 'red';
+    });
+    dogBreedContainer.appendChild(li);
+  }
+  
